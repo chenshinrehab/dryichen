1014b458a7f889140c0559098ccb2a31e734509f
jimmy <jimmyforjob2@gmail.com>
2026-01-09 00:05:59 +0800

    Update: 修改五十肩的治療內容

---
+++ public/robots.ts
@@ -0,0 +1,3 @@
+User-agent: *
+Allow: /
+Sitemap: https://quiet-medovik-fbddd0.netlify.app/sitemap.xml
---
+++ public/sitemap.ts
@@ -0,0 +1,10 @@
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+   <url>
+      <loc>https://quiet-medovik-fbddd0.netlify.app/</loc>
+      <lastmod>2024-01-08</lastmod>
+   </url>
+   <url>
+      <loc>https://quiet-medovik-fbddd0.netlify.app/treatments</loc>
+      <lastmod>2024-01-08</lastmod>
+   </url>
+   </urlset>
