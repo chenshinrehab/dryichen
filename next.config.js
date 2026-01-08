dbd6182bbba68e7b732f5a9a80a5f6a8a7c26436
jimmy <jimmyforjob2@gmail.com>
2026-01-08 10:58:38 +0800

    Fix: 移除 basePath 準備部署到 Netlify

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/dryichen', // 配合您的 GitHub Repo 名稱
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', 
+  // basePath: '/dryichen',  <-- 這行一定要刪掉或加雙斜線註解掉！
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
-  // 禁用一些可能导致问题的功能
-  distDir: 'out',
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+
